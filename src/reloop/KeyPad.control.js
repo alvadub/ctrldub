27684bf38a285b4f4d73393cfcbb85a6619321f9
Alvaro Cabrera <pateketrueke@gmail.com>
2017-02-01 21:29:19 -0600

    Minimal entry-point

--- src/reloop/KeyPad.control.js
+++ src/reloop/KeyPad.control.js
@@ -1,42 +1,9 @@
-/* global loadAPI, sendSysex, host */
+/* global loadAPI */
 /* eslint-disable no-new-func */
 
-import KeyPad from './_keypad';
-import defaultActions from './_default/actions';
-import defaultMappings from './_default/mappings';
+import run from './_run';
 
 loadAPI(1);
 
-let RL;
-
 // hack for exposing public symbols
-const GLOBAL = new Function('return this')();
-
-GLOBAL.DEBUG = true;
-
-GLOBAL.init = () => {
-  sendSysex(KeyPad.ID3);
-
-  const keys = host.getMidiInPort(0).createNoteInput('Keys', '?0????', '?1????', '?2????');
-  const pads = host.getMidiInPort(0).createNoteInput('Pads', '?4????');
-
-  keys.setShouldConsumeEvents(false);
-  pads.setShouldConsumeEvents(false);
-
-  RL = new KeyPad()
-    .initialize()
-    .add(defaultActions)
-    .set(defaultMappings);
-};
-
-GLOBAL.exit = () => {
-  RL.teardown();
-};
-
-host.defineMidiPorts(1, 1);
-host.defineController(KeyPad.VENDOR, KeyPad.PRODUCT, KeyPad.VERSION, KeyPad.GUID);
-
-host.addDeviceNameBasedDiscoveryPair([KeyPad.ID1], [KeyPad.ID1]);
-host.addDeviceNameBasedDiscoveryPair([KeyPad.ID2], [KeyPad.ID2]);
-
-host.defineSysexIdentityReply(KeyPad.ID4);
+run(new Function('return this')());
---
+++ src/reloop/_run.js
@@ -0,0 +1,38 @@
+/* global sendSysex, host */
+
+import KeyPad from './_keypad';
+import defaultActions from './_default/actions';
+import defaultMappings from './_default/mappings';
+
+export default GLOBAL => {
+  let RL;
+
+  GLOBAL.DEBUG = true;
+
+  GLOBAL.init = () => {
+    sendSysex(KeyPad.ID3);
+
+    const keys = host.getMidiInPort(0).createNoteInput('Keys', '?0????', '?1????', '?2????');
+    const pads = host.getMidiInPort(0).createNoteInput('Pads', '?4????');
+
+    keys.setShouldConsumeEvents(false);
+    pads.setShouldConsumeEvents(false);
+
+    RL = new KeyPad()
+      .initialize()
+      .add(defaultActions)
+      .set(defaultMappings);
+  };
+
+  GLOBAL.exit = () => {
+    RL.teardown();
+  };
+
+  host.defineMidiPorts(1, 1);
+  host.defineController(KeyPad.VENDOR, KeyPad.PRODUCT, KeyPad.VERSION, KeyPad.GUID);
+
+  host.addDeviceNameBasedDiscoveryPair([KeyPad.ID1], [KeyPad.ID1]);
+  host.addDeviceNameBasedDiscoveryPair([KeyPad.ID2], [KeyPad.ID2]);
+
+  host.defineSysexIdentityReply(KeyPad.ID4);
+};
